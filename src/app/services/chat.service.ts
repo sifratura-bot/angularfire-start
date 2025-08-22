b41b149c5a16609bde42f121df583c091de3e2ee
Ondrej Bruna <ondrej.bruna@gmail.com>
2025-08-22 16:27:31 +0200

    <A> Reading messages.

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -141,9 +141,13 @@ export class ChatService {
   };
 
   // Loads chat messages history and listens for upcoming ones.
-  loadMessages = () => {
-    return null as unknown;
-  };
+  loadMessages = () =>  {
+    // Create the query to load the last 12 messages and listen for new ones.
+    const recentMessagesQuery = query(collection(this.firestore, 'messages'), orderBy('timestamp', 'desc'), limit(12));
+    // Start listening to the query.
+    return collectionData(recentMessagesQuery);
+  }
+
 
   // Saves a new message containing an image in Firebase.
   // This first saves the image in Firebase storage.
